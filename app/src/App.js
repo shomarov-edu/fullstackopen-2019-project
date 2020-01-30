75e656fd4b9f3aaed4d44df2c30a60702d29cb29
Artur Shomarov <shomarov@protonmail.com>
2020-01-30 14:22:48 +0200

    Install Material-UI framework

--- app/src/App.js
+++ app/src/App.js
@@ -8,6 +8,8 @@ import {
   // eslint-disable-next-line no-unused-vars
   withRouter
 } from 'react-router-dom'
+import { Helmet } from 'react-helmet'
+import CssBaseline from '@material-ui/core/CssBaseline'
 import recipeService from './services/recipes'
 import Login from './components/Login'
 import Recipes from './components/Recipes'
@@ -32,37 +34,48 @@ function App() {
 
   return (
     <div className="App">
-      <Router>
-        <div>
-          <Link style={padding} to="/">
-            home
-          </Link>
-          <Link style={padding} to="/recipes">
-            recipes
-          </Link>
-          <Link style={padding} to="/new">
-            new
-          </Link>
-          <Link style={padding} to="/login">
-            login
-          </Link>
-        </div>
-        <h1>Recipes app</h1>
-        <Route exact path="/login" render={() => <Login />} />
-        <Route
-          exact
-          path="/recipes"
-          render={() => <Recipes recipes={recipes} />}
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Full Stack Open 2019-2020 Project</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
         />
-        <Route exact path="/new" render={() => <NewRecipeForm />} />
-        <Route
-          exact
-          path="/recipes/:id"
-          render={({ match }) => (
-            <Recipe recipe={recipeById(match.params.id)} />
-          )}
-        />
-      </Router>
+      </Helmet>
+      <React.Fragment>
+        <CssBaseline />
+        <Router>
+          <div>
+            <Link style={padding} to="/">
+              home
+            </Link>
+            <Link style={padding} to="/recipes">
+              recipes
+            </Link>
+            <Link style={padding} to="/new">
+              new
+            </Link>
+            <Link style={padding} to="/login">
+              login
+            </Link>
+          </div>
+          <h1>Recipes app</h1>
+          <Route exact path="/login" render={() => <Login />} />
+          <Route
+            exact
+            path="/recipes"
+            render={() => <Recipes recipes={recipes} />}
+          />
+          <Route exact path="/new" render={() => <NewRecipeForm />} />
+          <Route
+            exact
+            path="/recipes/:id"
+            render={({ match }) => (
+              <Recipe recipe={recipeById(match.params.id)} />
+            )}
+          />
+        </Router>
+      </React.Fragment>
     </div>
   )
 }
