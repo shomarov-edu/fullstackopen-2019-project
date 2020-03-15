df10a560f164264e8bec719769d61a93da530267
Artur Shomarov <shomarov@protonmail.com>
2020-03-15 15:18:52 +0200

    small fixes

--- client/src/index.js
+++ client/src/index.js
@@ -21,7 +21,7 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
-const httpLink = new HttpLink({ uri: 'http://localhost:4000/graphql' });
+const httpLink = new HttpLink({ uri: 'http://localhost:4000/' });
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
